530d2211db013da202cd87b5032389d24eb5674b
Prisca C <prisca.castanier@gmail.com>
2022-06-17 19:23:09 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+function Header() {
+  const header = [
+    {
+      title: 'Accueil',
+      url: './index.html'
+    },
+    {
+      title: 'À propos',
+      url: './index.html#about',
+    },
+    {
+      title: 'Portfolio'
+    }
+  ]
+  return(
+
+  )
+}
+
+const App = () => {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -20,6 +39,7 @@ function App() {
       </header>
     </div>
   );
+
 }
 
 export default App;
