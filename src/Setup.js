6fd70d814457969c90b7a8a658fa7fb8e3d18e59
Prisca C <prisca.castanier@gmail.com>
2022-06-21 21:27:20 +0200

    Added component for the next section

---
+++ src/Setup.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Setup = () => {
+    return (
+        <div>
+            <Container fluid>
+            
+            </Container>
+
+            <Container fluid>
+            
+            </Container>
+
+            <Container fluid>
+            
+            </Container>
+        </div>
+    )
+}
+
+export default Setup;
